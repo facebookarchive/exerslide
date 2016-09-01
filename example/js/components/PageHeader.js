5498e8dda1bbc554eed3033b6389ba1ef7c41aa7
Greg Gamel <ggamel@users.noreply.github.com>
2016-08-31 19:28:09 -0500

    Fix header title link to go to project home
    Resolves the 404 pointing at the top-level GH Pages root.

--- example/js/components/PageHeader.js
+++ example/js/components/PageHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function() {
   return (
     <div className="page-header flex-item-fix">
-      <div className="title pull-left"><a href="/">exerslide</a></div>
+      <div className="title pull-left"><a href="/exerslide">exerslide</a></div>
       <div className="navigation pull-right">
         <a href="#/docs">Docs</a>
         <a href="https://github.com/facebookincubator/exerslide">GitHub</a>
